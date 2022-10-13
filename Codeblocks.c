9874096de8e7cf4476cbc291858ab212fc68fcdc
JOZ1KOR <joz1kor@bosch.com>
2022-10-13 09:56:14 +0530

    Assignment

---
+++ Codeblocks.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "math.h"
+
+int main()
+{
+    int a,b, add,subtract, multiply ;
+  float divide();
+  printf("Enter two integers\n");
+  scanf("%d%d", &a, &b);
+  add = a+b;
+  subtract= a-b;
+  multiply= a*b;
+  divide = a/(float)b;
+  printf("Sum = %d\n", add);
+  printf("Difference = %d\n", subtract);
+    printf("Multiplication = %d\n", multiply);
+      printf("Division = %.2f\n", divide);
+
+      return 0;
+}
+
